b899bb71794bfa2ab30ddaf52598164577243b49
Ievgen Kotvizky <ekotvizky@gmail.com>
2018-10-29 15:02:12 +0200

    DB descriotion class;

--- Juja-core/src/ua/com/juja/SqlCmd/DBTypeConst.java
+++ Juja-core/src/ua/com/juja/SqlCmd/DBTypeConst.java
@@ -1,25 +1,43 @@
 package ua.com.juja.SqlCmd;
 
 public abstract class DBTypeConst {
-    public static String SERVER_NAME;
-    public static String SERVER_PORT;
+    public final String ServerName;
+    public final String ServerPort;
+    public final String DriverClassName;
+    public final String DriverStringName;
 
     abstract String getConnectionString(String database, String username, String password);
+
+    public DBTypeConst(String serverName, String serverPort,
+                       String driverClassName, String driverStringName) {
+        ServerName = serverName;
+        ServerPort = serverPort;
+        DriverClassName = driverClassName;
+        DriverStringName = driverStringName;
+    }
+
 
 }
 
 final class DBTypeConstPosgree extends DBTypeConst {
-    public static final String SERVER_NAME = "localhost";
-    public static final String SERVER_PORT = "5432";
+
+    public DBTypeConstPosgree(){
+        super(
+        "localhost",
+        "5432",
+        "org.postgresql.Driver",
+        "postgresql"
+                );
+    }
 
     public String getConnectionString(String database, String username, String password){
-        return String.format("jdbc:postgresql://%1$s:%2$s"
-                        + "/%3$s"
-                        + "?user=%4$s"
-                        + "&password=%5$s",
-
-                SERVER_NAME,
-                SERVER_PORT,
+        return String.format("jdbc:%1$s://%2$s:%3$s"
+                        + "/%4$s"
+                        + "?user=%5$s"
+                        + "&password=%6$s",
+                DriverStringName,
+                ServerName,
+                ServerPort,
                 database,
                 username,
                 password
@@ -28,16 +46,23 @@ final class DBTypeConstPosgree extends DBTypeConst {
 }
 
 final class DBTypeConstMssql extends DBTypeConst {
-    public static final String SERVER_NAME = "localhost";
-    public static final String SERVER_PORT = "&password";
+    public DBTypeConstMssql(){
+        super(
+            "sqlserver",
+            "10.1.32.56",
+            "1433",
+            "com.microsoft.sqlserver.jdbc.SQLServerDriver"
+        );
+    }
 
     public String getConnectionString(String database, String username, String password){
 
-        return String.format("\"jdbc:sqlserver://%1$s:%2$s"
-                        +";databaseName=%3$s"
+        return String.format("jdbc:%1$s://%2$s:%3$s"
+                        +";databaseName=%4$s"
                         +";integratedSecurity=true",
-                SERVER_NAME,
-                SERVER_PORT,
+                DriverClassName,
+                ServerName,
+                ServerPort,
                 database,
                 username,
                 password
