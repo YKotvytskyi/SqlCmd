95398501d0300f641024529c25e7a359a425071e
Ievgen Kotvizky <ekotvizky@gmail.com>
2018-11-13 10:09:22 +0200

    new structure

---
+++ Juja-core/src/ua/com/juja/SqlCmd/model/DatabaseManagerInterface.java
@@ -0,0 +1,4 @@
+package ua.com.juja.SqlCmd.model;
+
+public interface DatabaseManagerInterface {
+}
---
+++ Juja-core/src/ua/com/juja/SqlCmd/model/DdTypes/DBTypeConstPosgree.java
@@ -0,0 +1,29 @@
+package ua.com.juja.SqlCmd.DdTypes;
+
+public final class DBTypeConstPosgree extends DBTypeConst {
+
+    public DBTypeConstPosgree(){
+        super(
+                "localhost",
+                "5432",
+                "org.postgresql.Driver",
+                "postgresql",
+                "public"
+        );
+    }
+
+    public String getConnectionString(String database, String username, String password){
+        return String.format("jdbc:%1$s://%2$s:%3$s"
+                        + "/%4$s"
+                        + "?user=%5$s"
+                        + "&password=%6$s",
+                DriverStringName,
+                ServerName,
+                ServerPort,
+                database,
+                username,
+                password
+        );
+    }
+
+}
